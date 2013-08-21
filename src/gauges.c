8750077949ea8bb3cb21ab70b89a3698002b29d6
Peter Hardy <peter@hardy.dropbear.id.au>
2013-08-21 20:37:44 +1000

    Added initial dial layer.

--- src/gauges.c
+++ src/gauges.c
@@ -2,7 +2,8 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-#define HAND_LENGTH 67
+#define DIAL_RADIUS 70
+#define HAND_LENGTH 69
 
 #define MY_UUID { 0x52, 0x39, 0x16, 0x5D, 0x4D, 0x79, 0x4D, 0xEF, 0xA5, 0x5F, 0xB7, 0x93, 0x07, 0xD7, 0x4D, 0x29 }
 PBL_APP_INFO(MY_UUID,
@@ -12,9 +13,28 @@ PBL_APP_INFO(MY_UUID,
              APP_INFO_WATCH_FACE);
 
 Window window;
-Layer time_layer;
+Layer dial_layer, time_layer;
 
 GPoint hour_centre, minute_centre;
+
+void dial_layer_update(Layer *me, GContext *ctx) {
+  // background
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, me->bounds, 0, GCornerNone);
+  // dials
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, hour_centre, DIAL_RADIUS);
+  graphics_fill_circle(ctx, minute_centre, DIAL_RADIUS);
+  // centres
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_circle(ctx, hour_centre, 4);
+  graphics_fill_circle(ctx, minute_centre, 4);
+
+  // Start of dial markings
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_circle(ctx, hour_centre, DIAL_RADIUS-5);
+  graphics_draw_circle(ctx, minute_centre, DIAL_RADIUS-5);
+}
 
 void time_layer_update(Layer *me, GContext *ctx) {
   PblTm now;
@@ -59,6 +79,9 @@ void handle_init(AppContextRef ctx) {
   window_init(&window, "Root window");
   window_stack_push(&window, true /* Animated */);
 
+  layer_init(&dial_layer, window.layer.bounds);
+  dial_layer.update_proc = dial_layer_update;
+  layer_add_child(&window.layer, &dial_layer);
   layer_init(&time_layer, window.layer.bounds);
   time_layer.update_proc = time_layer_update;
   layer_add_child(&window.layer, &time_layer);
