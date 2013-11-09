84421678ccde5ee252d34a9811322341403d60c4
Peter Hardy <peter@hardy.dropbear.id.au>
2013-11-09 16:58:27 +1100

    Store config options in Pebble app, send and display in config page.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -5,12 +5,20 @@ Pebble.addEventListener("ready", function() {
 });
 
 Pebble.addEventListener("showConfiguration", function() {
+    var options = JSON.parse(window.localStorage.getItem('options'));
+    console.log("read options: " + JSON.stringify(options));
     console.log("showing configuration");
-    Pebble.openURL('http://hardy.dropbear.id.au/pebble/Gauges/config/2-1.html');
+    var uri = 'http://hardy.dropbear.id.au/pebble/Gauges/config/2-1.html?' + 'btdisco-notif=' + encodeURIComponent(options['btdisco-notif']) + '&lowbat-notif=' + encodeURIComponent(options['lowbat-notif']);
+    Pebble.openURL(uri);
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
     console.log("configuration closed");
-    var options = JSON.parse(decodeURIComponent(e.response));
-    console.log("Options = " + JSON.stringify(options));
+    if (e.response != '') {
+	var options = JSON.parse(decodeURIComponent(e.response));
+	console.log("storing options: " + JSON.stringify(options));
+	window.localStorage.setItem('options', JSON.stringify(options));
+    } else {
+	console.log("no options received");
+    }
 });
